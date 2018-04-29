a83ef14818302dc4a2618b0ef14c1fce2f97bd42
KRUNO-PC\Krunoslav <krunoslavsterle@gmail.com>
2018-04-29 10:46:42 +0200

    - Frontend functionality

--- TwoNote/src/TwoNote.Web/Controllers/NotebookController.cs
+++ TwoNote/src/TwoNote.Web/Controllers/NotebookController.cs
@@ -42,6 +42,36 @@ namespace TwoNote.Web.Controllers
             return PartialView("_NotebookSection", vm);
         }
 
+        [HttpPost]
+        [ActionName("NewNotebook")]
+        public async Task<JsonResult> NewNotebookAsync(string notebookName)
+        {
+            if (String.IsNullOrEmpty(notebookName)) throw new ArgumentNullException("[notebookName] is null or empty.");
+
+            var entity = new NotebookEntity()
+            {
+                Id = Guid.NewGuid(),
+                Name = notebookName,
+                DateCreated = DateTime.UtcNow,
+                DateUpdated = DateTime.UtcNow
+            };
+
+            var result = await notebookRepository.AddAsync(entity);
+            return Json(new { success = true, responseText = result.Id.ToString() });
+        }
+
+        [HttpPost]
+        [ActionName("DeleteNotebook")]
+        public async Task<IActionResult> DeleteNotebookAsync(Guid notebookId)
+        {
+            if (notebookId == Guid.Empty) throw new FormatException("Invalid format exception for [notebookId]");
+
+            var entity = await notebookRepository.GetByIdAsync(notebookId);
+            await notebookRepository.DeleteAsync(entity);
+
+            return Ok();
+        }
+
         [HttpGet]
         [ActionName("Pages")]
         public async Task<IActionResult> PagesPartialAsync(Guid notebookId, Guid? pageId)
@@ -52,6 +82,63 @@ namespace TwoNote.Web.Controllers
             var vm = MapNotebookEntityToNotebookViewModel(notebook, pageId);
 
             return PartialView("_PageSection", vm);
+        }
+
+        [HttpPost]
+        [ActionName("NewPage")]
+        public async Task<JsonResult> NewPageAsync(Guid notebookId, string pageName)
+        {
+            if (Guid.Empty == notebookId) throw new ArgumentNullException("[notebookId] is empty guid.");
+            if (String.IsNullOrEmpty(pageName)) throw new ArgumentNullException("[pageName] is null or empty.");
+
+            var entity = new PageEntity()
+            {
+                Id = Guid.NewGuid(),
+                NotebookId = notebookId,
+                Name = pageName,
+                Content = String.Empty,
+                DateCreated = DateTime.UtcNow,
+                DateUpdated = DateTime.UtcNow
+            };
+
+            var result = await pageRepository.AddAsync(entity);
+            return Json(new { success = true, responseText = result.Id.ToString() });
+        }
+
+        [HttpPost]
+        [ActionName("DeletePage")]
+        public async Task<IActionResult> DeletePageAsync(Guid pageId)
+        {
+            if (pageId == Guid.Empty) throw new FormatException("Invalid format exception for [pageId]");
+
+            var entity = await pageRepository.GetByIdAsync(pageId);
+            await pageRepository.DeleteAsync(entity);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [ActionName("GetPageContent")]
+        public async Task<IActionResult> GetPageContentAsync(Guid pageId)
+        {
+            if (pageId == Guid.Empty) throw new FormatException("Invalid format exception for [pageId]");
+            var entity = await pageRepository.GetByIdAsync(pageId);
+            return Ok(entity.Content);
+        }
+
+        [HttpPost]
+        [ActionName("UpdatePage")]
+        public async Task<IActionResult> UpdatePageAsync(Guid pageId, string content)
+        {
+            if (pageId == Guid.Empty) throw new FormatException("Invalid format exception for [pageId]");
+            if (content == null) throw new ArgumentNullException("[content] is null.");
+
+            var entity = await pageRepository.GetByIdAsync(pageId);
+            entity.Content = content;
+            entity.DateUpdated = DateTime.UtcNow;
+            await pageRepository.UpdateAsync(entity);
+
+            return Json(new { success = true, responseText = "Updated" });
         }
 
         private IndexViewModel CreateIndexViewModel(IEnumerable<NotebookEntity> notebookEntities, Guid? notebookId = null)
